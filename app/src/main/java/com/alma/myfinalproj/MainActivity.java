55e4075b8ccb99c50b813cd804035dd17fe05229
Student-2024 <student-2024@ashalim.org>
2025-11-13 13:42:06 +0200

    EOD 13.11.2025

---
+++ app/src/main/java/com/alma/myfinalproj/MainActivity.java
@@ -0,0 +1,32 @@
+package com.alma.myfinalproj;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button btnsu;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        btnsu=findViewById(R.id.btnsu);
+        btnsu.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        Intent GO
+    }
+}
---
+++ app/src/main/java/com/alma/myfinalproj/model/Item.java
@@ -0,0 +1,100 @@
+package com.alma.myfinalproj.model;
+
+public class Item {
+    protected String id;
+    protected String name;
+    protected String type;
+    protected String size;
+    protected Double price;
+    protected String details;
+    protected String pic;
+
+
+    public Item(String id, String name, String type, String size, Double price, String details, String pic) {
+        this.id = id;
+        this.name = name;
+        this.type = type;
+        this.size = size;
+        this.price = price;
+        this.details = details;
+        this.pic = pic;
+    }
+
+
+    public Item() {
+    }
+
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public String getDetails() {
+        return details;
+    }
+
+    public void setDetails(String details) {
+        this.details = details;
+    }
+
+    public String getPic() {
+        return pic;
+    }
+
+    public void setPic(String pic) {
+        this.pic = pic;
+    }
+
+
+
+    @Override
+    public String toString() {
+        return "Item{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", type='" + type + '\'' +
+                ", size='" + size + '\'' +
+                ", price=" + price +
+                ", details='" + details + '\'' +
+                ", pic='" + pic + '\'' +
+                '}';
+    }
+
+
+}
